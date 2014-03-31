03df4eba7620d460fb8bedb7b2294a99eb760333
Fabricio Flores <fabrifloresg@gmail.com>
2014-03-30 19:27:33 -0500

    Update builder_mixin.rb

--- lib/nested_form/builder_mixin.rb
+++ lib/nested_form/builder_mixin.rb
@@ -100,6 +100,39 @@ module NestedForm
         super
       end
     end
+    
+    def link_to_add_hmt(*args, &block)
+      options = args.extract_options!.symbolize_keys
+      association = args.pop
+      association_two = args.pop
+ 
+      unless object.respond_to?("#{association}_attributes=")
+        raise ArgumentError, "Invalid association. Make sure that accepts_nested_attributes_for is used for #{association.inspect} association."
+      end
+ 
+      model_object = options.delete(:model_object) do
+        reflection = object.class.reflect_on_association(association)
+        reflection.klass.new
+      end
+ 
+      options[:class] = [options[:class], "add_nested_fields"].compact.join(" ")
+      options["data-association"] = association
+      options["data-blueprint-id"] = fields_blueprint_id = fields_blueprint_id_for(association)
+      args << (options.delete(:href) || "javascript:void(0)")
+      args << options
+ 
+      model_object.send("build_#{association_two}".to_sym)
+ 
+      @fields ||= {}
+      @template.after_nested_form(fields_blueprint_id) do
+        blueprint = {:id => fields_blueprint_id, :style => 'display: none'}
+        block, options = @fields[fields_blueprint_id].values_at(:block, :options)
+        options[:child_index] = "new_#{association}"
+        blueprint[:"data-blueprint"] = fields_for(association, model_object, options, &block).to_str
+        @template.content_tag(:div, nil, blueprint)
+      end
+      @template.link_to(*args, &block)
+    end
 
     private
 
